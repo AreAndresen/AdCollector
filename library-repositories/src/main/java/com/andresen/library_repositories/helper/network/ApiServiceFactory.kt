be33638910b449556e09ce67528fc14947e75871
Are Andresen <are.sebastian.andresen@nrk.no>
2023-06-18 18:01:42 +0200

    remove unnecessary code

--- library-repositories/src/main/java/com/andresen/library_repositories/helper/network/ApiServiceFactory.kt
+++ library-repositories/src/main/java/com/andresen/library_repositories/helper/network/ApiServiceFactory.kt
@@ -13,20 +13,3 @@ interface ApiServiceFactory {
             .build()
     }
 }
-
-
-internal inline fun <reified T> Moshi.convertToJson(instance: T): String? = try {
-    adapter(T::class.java).toJson(instance)
-} catch (ex: Exception) {
-    Timber.e(ex, "Failed converting ${T::class.simpleName} to json")
-    null
-}
-
-internal inline fun <reified T> Moshi.convertFromJson(blob: String?): T? = blob?.let {
-    try {
-        adapter(T::class.java).fromJson(it)
-    } catch (ex: Exception) {
-        Timber.e(ex, "Failed converting ${T::class.simpleName} from json")
-        null
-    }
-}
